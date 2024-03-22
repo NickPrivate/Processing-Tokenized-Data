f4e286d1e1d6d7c36982968992039e7740ebca12
lapanaj2 <lapanaj2@gmail.com>
2024-03-21 18:49:55 -0700

    output

--- main.py
+++ main.py
@@ -1,30 +1,22 @@
-from modules.file_reader import FileReader
+import keyword
+import nltk
 from modules.preprocessor import Preprocessor
+from modules.file_reader import FileReader
 from modules.tokenizer import Tokenizer
 from modules.output_formatter import OutputFormatter
+# nltk.download('punkt')
 
-
-def main():
-    file_path = 'tests/input.txt'  # Adjust the path as necessary
-    
-    # Step 1: Read file
-    file_reader = FileReader(file_path)
+def output():
+    file = "input.txt"
+    file_reader = FileReader(file)
     lines = file_reader.read_file()
-    
-    # Step 2: Preprocess - remove comments and excess whitespace
+        
     preprocessor = Preprocessor()
     processed_lines = preprocessor.preprocess(lines)
-    
-    # Future steps: Tokenizing and formatting output
-    # tokenizer = Tokenizer()
-    # tokens = tokenizer.tokenize(processed_lines)
-    
-    # output_formatter = OutputFormatter()
-    # output_formatter.format(tokens)
 
-    # For now, just print the processed lines to verify
-    for line in processed_lines:
-        print(line)
+    tokenizer = Tokenizer()
+    keywords, literals, operators, identifiers = tokenizer.tokenize(processed_lines)
 
-if __name__ == "__main__":
-    main()
+    OutputFormatter().output(processed_lines, keywords, literals, operators, identifiers)
+
+output()
--- modules/output_formatter.py
+++ modules/output_formatter.py
@@ -1,4 +1,22 @@
+from tabulate import tabulate
+
 class OutputFormatter:
     #Step 4 formats and prints the code and tokenized output in a nice format
-    def output(self):
+    def output(self, lines, keywords, literals, operators, identifiers):
+        for line in lines:
+            print(line)
+        print()
+
+        keywords = ', '.join(keywords)
+        literals = ', '.join(literals)
+        operators = ', '.join(operators)
+        identifiers = ', '.join(identifiers)
+
+        table = [
+            ['Keywords', keywords],
+            ['Literals', literals],
+            ['Operators', operators],
+            ['Identifiers', identifiers]
+        ]
+        print(tabulate(table, headers=['Category', 'Tokens'], tablefmt='fancy_grid'))
         pass
